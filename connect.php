c7c85ecc60531514b4ccda240d9a09253884f316
BEGUE <beguethomas439@gmail.com>
2022-03-30 11:15:24 +0400

    new modification

--- connect.php
+++ connect.php
@@ -1,9 +1,20 @@
 <?php
 
-    $con = new mysqli('localhost', 'thomasB', 'thomassql', 'Vap Factory');
-
-    if (!$con){
-        die(mysqli_error($con));
-    } 
+    try {
+        // dev configuration
+        if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "development") == 0) {
+            $con = new mysqli(
+                'localhost',
+                'thomasB',
+                'thomassql',
+                'Vap Factory'
+            );
+        }
+        if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
+            $con = new mysqli('109.234.164.161', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD'], 'sc1lgvu9627_begue-thomas.sprint-06');
+        }
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
 
 ?>
